02ef34ebbe5db8a46f2cf0fe1d2e0ecf420d94e0
Rafa Ramírez <raffe90@gmail.com>
2015-12-05 16:44:28 +0100

    Adds navbar with basic styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#index'
+
+ get 'help' => 'static_pages#help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
